9da3aa4daafb077a000116b9c9bd5669119b9f20
Josh Kalov <jkalov@gmail.com>
2017-01-05 10:11:32 -0600

    ..

--- deploy/hourly.ps1
+++ deploy/hourly.ps1
@@ -15,3 +15,6 @@ $env_path = Join-Path $env:HOME \deploy\envs\cookcountyilgov.env.ps1
 . $env_path
 node $env:HOME\bin\analytics --output $env:DATA_PATH --frequency=hourly --slim --verbose
 . $env:S3BROWSER sync $env:S3BROWSER_ACCOUNT $env:DATA_PATH $env:AWS_PATH ncdhs
+
+"Ready to exit..."
+exit
