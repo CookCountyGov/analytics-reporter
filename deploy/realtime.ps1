bcfb685405f4d31ea72ab569e0ab8bc165c2b601
Josh Kalov <jkalov@gmail.com>
2017-01-31 15:18:20 -0600

    fix realtime pages csv link

--- deploy/realtime.ps1
+++ deploy/realtime.ps1
@@ -8,14 +8,14 @@ $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $env_path = Join-Path $env:HOME \deploy\envs\dataportal.env.ps1
 . $env_path
 node $env:HOME\bin\analytics --output $env:DATA_PATH --frequency=realtime --slim --verbose
-node $env:HOME\bin\analytics --output $env:DATA_PATH --only=all-pages-realtime --slim --verbose --csv
+node $env:HOME\bin\analytics --output $env:DATA_PATH --only=top-pages-realtime --slim --verbose --csv
 . $env:S3BROWSER sync $env:S3BROWSER_ACCOUNT $env:DATA_PATH $env:AWS_PATH ncdhs
 
 # cookcountyil.gov
 $env_path = Join-Path $env:HOME \deploy\envs\cookcountyilgov.env.ps1
 . $env_path
 node $env:HOME\bin\analytics --output $env:DATA_PATH --frequency=realtime --slim --verbose
-node $env:HOME\bin\analytics --output $env:DATA_PATH --only=all-pages-realtime --slim --verbose --csv
+node $env:HOME\bin\analytics --output $env:DATA_PATH --only=top-pages-realtime --slim --verbose --csv
 . $env:S3BROWSER sync $env:S3BROWSER_ACCOUNT $env:DATA_PATH $env:AWS_PATH ncdhs
 "Ready to exit..."
 exit
